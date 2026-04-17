ca4ea5d7e4ee7f0452a97a0174cb1e9547b35d1d
Eshwar <eshwar@gmail.com>
2026-04-17 09:58:39 +0000

    healthcheck

---
+++ healthcheck/checker/checker.go
@@ -0,0 +1,25 @@
+package checker
+
+
+import (
+
+  "fmt"
+  "github.com/fatih/color"
+  "github.com/EshwarSatale/devops-cohort-go/healthcheck/checker"
+)
+
+func PrintStatus(s models.Service){
+  status := "Healthy"
+  
+  if s.Healthy == false {
+    status = "Unhealthy"
+    
+    msg :=  fmt.Sprintf("Name: %s | Port: %d  | %s ", s.Name, s.Port, status)
+    color.Red(msg)
+
+   }else{
+    msg :=  fmt.Sprintf("Name: %s | Port: %d  | %s ", s.Name, s.Port, status)
+    color.HiGreen(msg)
+   }
+
+}
--- healthcheck/main.go
+++ healthcheck/main.go
@@ -1,14 +1,25 @@
+
 package main
 
-import (
 
- "github.com/fatih/color"
- "fmt"
+import (
+	"fmt"
+	"github.com/EshwarSatale/devops-cohort-go/healthcheck/models"
+	"github.com/EshwarSatale/devops-cohort-go/healthcheck/checker"
 )
 
-func main(){
+func main() {
 
-	color.Green("application is healthy")
-        color.Red("worker service is down")
-        fmt.Println("plain output")
+	fmt.Println("Devops Healthcheck!")
+
+	services := []models.Service{
+   	   {Name: "gateway",  Port: 8080  , Healthy: true },
+    	   {Name: "postgres",  Port: 5432 , Healthy: false },
+    	   {Name: "frontend",  Port:  443 , Healthy:  true },
+       }
+
+	for _, svc  := range services {
+
+		checker.PrintStatus(svc)
+	}
 }
